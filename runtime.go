b94776dcab37c65ac20f880be362f9b7a294dde8
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-03-19 21:13:50 -0400

    no linkname for funcPC that no longer exists

--- runtime.go
+++ runtime.go
@@ -1,6 +1,7 @@
 package dl
 
 import (
+	"reflect"
 	"strings"
 	"syscall"
 	"unsafe"
@@ -9,12 +10,14 @@ import (
 //go:linkname syscall_syscall syscall.syscall
 //go:linkname syscall_syscall6 syscall.syscall6
 //go:linkname runtime_gostring runtime.gostring
-//go:linkname funcPC runtime.funcPC
 
 func syscall_syscall(fn, a1, a2, a3 uintptr) (r1, r2, err uintptr)              // runtime/sys_darwin.go
 func syscall_syscall6(fn, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2, err uintptr) // runtime/sys_darwin.go
 func runtime_gostring(p *byte) string                                           // runtime/string.go
-func funcPC(f interface{}) uintptr                                              // runtime/proc.go
+
+func funcPC(f interface{}) uintptr {
+	return reflect.ValueOf(f).Pointer()
+}
 
 func cstring(s string) (*byte, error) {
 	if strings.IndexByte(s, 0) != -1 {
